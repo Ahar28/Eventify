6040509c22a5ae212cfe690e63dde304db1c2106
Keyur Khant <kpkhant007@yahoo.com>
2024-02-27 13:42:54 -0400

    feat: Navbar and Footer

---
+++ client-app/src/assets/home/index.js
@@ -0,0 +1,4 @@
+import logo from "./logo.png";
+import logo_2 from "./logo_2.png";
+
+export { logo, logo_2 };
---
+++ client-app/src/components/Container/index.tsx
@@ -0,0 +1,11 @@
+import React, { ReactNode } from 'react';
+
+interface ContainerProps {
+  children: ReactNode;
+}
+
+const Container: React.FC<ContainerProps> = ({ children }) => {
+  return <div className="md:w-11/12 w-full md:px-0 px-3 mx-auto">{children}</div>;
+};
+
+export default Container;
