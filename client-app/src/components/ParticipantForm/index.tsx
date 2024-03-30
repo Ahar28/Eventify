f75327b6c613fdd523b85ccc7a88384bcf249bce
Aharnish Maheshbhai Solanki <ah910744@dal.ca>
2024-03-30 02:24:04 -0300

    removed material UI styling from participant form

--- client-app/src/components/ParticipantForm/index.tsx
+++ client-app/src/components/ParticipantForm/index.tsx
@@ -1,9 +1,7 @@
 // src/components/ParticipantForm/index.tsx
 
 import React, { useState } from 'react';
-import { TextField, Grid, Typography } from '@mui/material';
 import Container from '../Container';
-import Button from '../UI/Button';
 import SectionTitle from '../../components/Landing/SectionTitle';
 
 interface Participant {
@@ -57,6 +55,7 @@ const ParticipantForm: React.FC<ParticipantFormProps> = ({ ticketQuantities,tick
     };
 
  return (
+  <Container>
     <div className="max-w-7xl mx-auto px-4 py-8">
       <SectionTitle title="Registration Information" />
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6 ">
@@ -65,34 +64,38 @@ const ParticipantForm: React.FC<ParticipantFormProps> = ({ ticketQuantities,tick
           <form onSubmit={handleSubmit} className="space-y-6">
             {participants.map((participant, index) => (
               <div key={index}>
-                <Typography variant="h6" component="h2" className="mb-2">Attendee {index + 1}</Typography>
+                <h2 className="text-xl font-bold mb-4">Attendee {index + 1}</h2>
                 {/* First Name and Last Name on the same line */}
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
-                <TextField
-                  fullWidth
-                  label="First Name"
-                  value={participant.firstName}
-                  onChange={(e) => handleInputChange(index, 'firstName', e.target.value)}
-                  margin="normal"
-                  className="mb-4"
-                />
-                <TextField
-                  fullWidth
-                  label="Last Name"
-                  value={participant.lastName}
-                  onChange={(e) => handleInputChange(index, 'lastName', e.target.value)}
-                  margin="normal"
-                  className="mb-4"
-                />
+                   <input
+                            type="text"
+                            name="FirstName"
+                            id="firstName"
+                            value={participant.firstName}
+                            onChange={(e) => handleInputChange(index, 'firstName', e.target.value)}
+                            className="border border-gray-300 text-md block w-full p-2.5 mt-6"
+                            placeholder="First Name"
+                        />
+                 <input
+                            type="text"
+                            name="LastName"
+                            id="lastName"
+                            value={participant.lastName}
+                            onChange={(e) => handleInputChange(index, 'lastName', e.target.value)}
+                            className="border border-gray-300 text-md block w-full p-2.5 mt-6"
+                            placeholder="Last Name"
+                        />
                 </div>
-                <TextField
-                  fullWidth
-                  label="Email"
-                  value={participant.email}
-                  onChange={(e) => handleInputChange(index, 'email', e.target.value)}
-                  margin="normal"
-                  className="mb-4"
-                />
+              
+                 <input
+                            type="text"
+                            name="Email"
+                            id="email"
+                            value={participant.email}
+                            onChange={(e) => handleInputChange(index, 'email', e.target.value)}
+                            className="border border-gray-300 text-md block w-full p-2.5 mt-6"
+                            placeholder="Email"
+                        />
               </div>
             ))}
             {/* <Button type="submit" variant="contained" color="primary">Submit</Button> */}
@@ -104,7 +107,7 @@ const ParticipantForm: React.FC<ParticipantFormProps> = ({ ticketQuantities,tick
 
         {/* Order Summary - Takes 1/3 space */}
         <div className="col-span-1 bg-gray-100 p-4 rounded-lg shadow-md">
-          <Typography variant="h6" className="font-semibold mb-4">Order Summary</Typography>
+          <h2 className="text-xl font-bold mb-4">Order Summary</h2>
           <div>
             {ticketOptions.map((ticket, index) => ticketQuantities[ticket.type] > 0 && (
               <div key={index} className="flex justify-between border-b py-2">
@@ -122,6 +125,7 @@ const ParticipantForm: React.FC<ParticipantFormProps> = ({ ticketQuantities,tick
         </div>
       </div>
     </div>
+    </Container>
   );
 };
 
