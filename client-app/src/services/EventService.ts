60f64b68f47153ee5538c065b6b61c7214050db6
ddoshi <dh442504@dal.ca>
2024-04-03 21:40:34 -0300

    Conflict Resolution

--- client-app/src/services/EventService.ts
+++ client-app/src/services/EventService.ts
@@ -12,23 +12,9 @@ export async function createEvent(eventData: Event) {
     console.error("Error adding event:", error);
     return null;
   }
-  try {
-    const response = await postData(JSON.stringify(eventData), "/event/create");
-    return response;
-  } catch (error) {
-    console.error("Error adding event:", error);
-    return null;
-  }
 }
 
 export async function getEventsByOrganizer(organizerId: string) {
-  try {
-    const response = await getData("/event/organizer/" + organizerId);
-    return response;
-  } catch (error) {
-    console.error("Error getting event:", error);
-    return null;
-  }
   try {
     const response = await getData("/event/organizer/" + organizerId);
     return response;
