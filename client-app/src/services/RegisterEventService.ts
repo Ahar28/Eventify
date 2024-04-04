0673ed364ca2e1f4d30770ee71c318a347c5e94c
Aharnish Maheshbhai Solanki <ah910744@dal.ca>
2024-04-04 00:13:19 -0300

    fix: URL update for deployed backend

--- client-app/src/services/RegisterEventService.ts
+++ client-app/src/services/RegisterEventService.ts
@@ -2,7 +2,8 @@ import {TicketRegistration} from "../components/ParticipantForm";
 import { deleteData, postData } from "./utils";
 import { json } from "stream/consumers";
 
-const API_URL = "http://localhost:8000/api";
+// const API_URL = "http://localhost:8000/api";
+const API_URL = "https://eventify-2f9d.onrender.com/api"; //for prod 
 
 export async function createEventRegistration(eventData: TicketRegistration) {
     try {
