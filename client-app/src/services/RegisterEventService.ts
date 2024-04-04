88594c0a388cb1ab5d3073578183976af36f0522
Aharnish Maheshbhai Solanki <ah910744@dal.ca>
2024-04-04 04:59:37 -0300

    fix: payment url update

--- client-app/src/services/RegisterEventService.ts
+++ client-app/src/services/RegisterEventService.ts
@@ -2,9 +2,6 @@ import {TicketRegistration} from "../components/ParticipantForm";
 import { deleteData, postData } from "./utils";
 import { json } from "stream/consumers";
 
-// const API_URL = "http://localhost:8000/api";
-const API_URL = "https://eventify-2f9d.onrender.com/api"; //for prod 
-
 export async function createEventRegistration(eventData: TicketRegistration) {
     try {
         const response = await postData(JSON.stringify(eventData), "/register/create");
